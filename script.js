512fc1ad1944c62dc6b03212779c56b83c6cf555
SerShiro <seregapolpro@gmail.com>
2022-09-21 23:54:49 +0300

    function encrypt

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputText = document.querySelector('#input')
 const inputK = document.querySelector('#k')
 const btn = document.querySelector('button')
 let number = 1
+const symbols = [`.`, `,`, `:`, `!`, `?`, `;`, `-`, ` `, `_`]
 
 function alphabet(number) {
     const alphAll = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я']
@@ -17,27 +18,32 @@ function alphabet(number) {
 console.log(alphabet(number))
 
 function encrypt(k) {
-    const alphabet = alphabet(number)
+    const alphabetet = alphabet(number)
     let input = inputText.value
 
     let encryptText = `` 
     for (let i = 0; i < input.length; i++) {
         let m = -1
-        for (let j = 0; j < alphabet.length; j++) {
-            if (input[i] === alphabet[j]) 
-            {
+        for (let j = 0; j < alphabetet.length; j++) {
+            symbols.forEach(item => {
+                if(item === input[i]) {
+                    encryptText += `${input[i]}`
+                    i++
+                }
+            })
+            if (input[i] === alphabetet[j]) {
                 m = j
+                encryptText += `${alphabetet[(m + k) % alphabet(number).length]}`
                 break
             }
         }
-        encryptText += `${alphabet[(m + k) % alphabet(number).length]}`
     }
     return encryptText
 }
 
 btn.addEventListener('click', (event) => {
     event.preventDefault()
-    let k = inputK.value
+    let k = Number(inputK.value)
     console.log(encrypt(k))
 })
 
