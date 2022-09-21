c4c97bfc5b64cb2b3099b2c0824964ca11b73cb9
SerShiro <seregapolpro@gmail.com>
2022-09-22 01:40:41 +0300

    complete encrypt function

--- script.js
+++ script.js
@@ -5,45 +5,44 @@ let number = 1
 const symbols = [`.`, `,`, `:`, `!`, `?`, `;`, `-`, ` `, `_`]
 
 function alphabet(number) {
-    const alphAll = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я']
-    const alphWithoutE = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я']
-    const alphWithoutQ = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я']
-    const alphWithoutEQ = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я']
+    const alphAll = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    const alphWithoutE = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    const alphWithoutQ = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    const alphWithoutEQ = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
     if (number === 1) return alphAll
     else if(number === 2) return alphWithoutE
     else if(number === 3) return alphWithoutQ
     else if(number === 4) return alphWithoutEQ
     // else alert('Ошибка')
 }
-console.log(alphabet(number))
 
 function encrypt(k) {
     const alphabetet = alphabet(number)
-    let input = inputText.value
+    let input = inputText.value.toLowerCase()
 
     let encryptText = `` 
     for (let i = 0; i < input.length; i++) {
-        let m = -1
-        for (let j = 0; j < alphabetet.length; j++) {
-            symbols.forEach(item => {
-                if(item === input[i]) {
-                    encryptText += `${input[i]}`
-                    i++
-                }
-            })
-            if (input[i] === alphabetet[j]) {
-                m = j
-                encryptText += `${alphabetet[(m + k) % alphabet(number).length]}`
-                break
-            }
+        const j = alphabetet.indexOf(input[i])
+        if (j === -1) {
+            encryptText += `${input[i]}`
+        }
+        else {
+            encryptText += `${alphabetet[(j + k) % alphabet(number).length]}`
         }
     }
     return encryptText
 }
 
+document.addEventListener('keypress', event => {
+    if (event.code === 13) {
+        let k = Number(inputK.value)
+        document.querySelector('#parEncrypt').textContent = encrypt(k)
+    }
+})
+
 btn.addEventListener('click', (event) => {
     event.preventDefault()
     let k = Number(inputK.value)
-    console.log(encrypt(k))
+    document.querySelector('#parEncrypt').textContent = encrypt(k)
 })
 
